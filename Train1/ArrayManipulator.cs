9138e96cf77e7a6874e1b1238c0d5a749623552a
Karasiq1337 <johnnovikov@gmail.com>
2023-06-22 18:40:44 +0500

    task

---
+++ Train1/ArrayManipulator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Train1
+{
+    static class ArrayManipulator
+    {
+        public static int[] Manipulate(int[] array)
+        {
+            int place = 0;
+            int[] resultArray = new int[array.Length];
+            for (int i = 0; i< array.Length; i++)
+            {
+                place = (i+4)%(array.Length);
+                resultArray[place] = array[i];
+            }
+            return RepaceMinAndMax(resultArray);
+        }
+
+        public static int[] ParseArray(string text) 
+        {
+            string[] array = text.Split(' ');
+            List<int> result = new List<int>();
+            int currentNumber = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                try
+                {
+                    currentNumber = Int32.Parse(array[i]);
+                    result.Add(currentNumber);
+                }
+                catch
+                {
+                    Console.WriteLine($"Unable to parse '{currentNumber}'");
+                }
+                
+            }
+            return result.ToArray();
+        }
+
+        public static int[] RepaceMinAndMax(int[] array)
+        {
+            int min = array[0];
+            int minIndex = 0;
+            int maxIndex = 0;
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    minIndex = i;
+                }
+            }
+            array[minIndex] = max;
+            array[maxIndex] = min;
+            return array;
+            
+        }
+    }
+}
---
+++ Train1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Train1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EbashButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите числа ниже:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // EbashButton
+            // 
+            this.EbashButton.Location = new System.Drawing.Point(395, 108);
+            this.EbashButton.Name = "EbashButton";
+            this.EbashButton.Size = new System.Drawing.Size(75, 23);
+            this.EbashButton.TabIndex = 5;
+            this.EbashButton.Text = "ЕБАШ";
+            this.EbashButton.UseVisualStyleBackColor = true;
+            this.EbashButton.Click += new System.EventHandler(this.EbashButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EbashButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button EbashButton;
+    }
+}
+
